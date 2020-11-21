843de881937298fe7a01a86921256f4ccfa5ef65
gregorioD <69092582+gregorioD@users.noreply.github.com>
2020-11-21 12:14:09 -0300

    fecha
    agregue para crear una fecha y pasar los meses de un str a un int

--- structs.cpp
+++ structs.cpp
@@ -3,6 +3,9 @@
 #include <fstream>
 #include <cstring>
 #include <ctime>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 using namespace std;
 
@@ -54,14 +57,71 @@ void GuardarPartida (PPartida match, int dif, int score, char tipo){
 	match -> puntaje = score;
 	match -> tipo = tipo;
 }
-void obtenerFecha (Pfecha date){
-	// no se como funciona time xd, asi que si podes fijate eso de los meses y anyo
-	time_t fechaact;
-	time(&fechaact);
-	date -> dia = (((fechaact / 60)/60)/24);
-	//date -> mes = ((((fechaact / 60)/60)/24)/30);
+void obtenerFecha(PFecha date){
 	
+	int dig0, dig1, dig2, dig3, mes, anio, dia;
+	char fecha[25], nombreMes[3];
+	time_t current_time;
+	current_time=time(NULL);
+	ctime(&current_time);
+	strcpy(fecha, ctime(&current_time));
+	dig0 = (int)fecha[8] - 48;
+	dig1 = (int)fecha[9] -48;
+	dia = dig0 * 10 + dig1;
+	date -> dia = dia;
+	for (int i = 0; i<3; i++){
+		nombreMes[i] = fecha[i+4];
+	}
+	mes = QueMesEs(nombreMes);
+	date -> mes = mes
+	dig0 = (int)fecha[20] - 48;
+	dig1 = (int)fecha[21] -48;
+	dig2 = (int)fecha[22] -48;
+	dig3 = (int)fecha[23] -48;
+	anio = dig0 * 1000 + dig1 * 100 + dig2 * 10 + dig3;
+	date -> anyo = anio;
+}
 	
+int QueMesEs(char mes[3]){
+	int rdo = -1;
+	if (mes[0]=='D'){
+		rdo = 12;
+	}else{
+		if (mes[0]=='N'){
+			rdo = 11;
+		}else{
+			if (mes[0]=='O'){
+				rdo = 10;
+			}else{
+				if (mes[0]=='S'){
+					rdo = 9;
+				}else{
+					if (mes[0]=='F'){
+						rdo = 2;
+					}else{
+						if(mes[0]=='A'){
+							if (mes[1]=='p') rdo = 4;
+							else rdo = 8;
+						}else{
+							if(mes[0]=='M'){
+								if(mes[2]=='r') rdo = 3;
+								else rdo = 5;
+							}else{
+								if(mes[0]=='J'){
+									if(mes[1]=='a') rdo = 1;
+									else{
+										if (mes[2]=='n') rdo = 6;
+										else rdo = 7;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	return  rdo;
 }
 
 void partidaAUsuario(PPartida match, PUsuario usr){
